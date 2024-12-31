7cd47b734dd01546347267838ce01a7ef2fd098b
Aayush <np03cs4a220238@heraldcollege.edu.np>
2024-12-31 16:29:08 +0545

    data collection script with data of anish & aayush

---
+++ saveData.py
@@ -0,0 +1,45 @@
+import serial
+import csv
+import time
+import os
+
+# Configure serial port (adjust as needed)
+ser = serial.Serial('COM3', 115200)  # Replace 'COM3' with your Arduino's serial port
+csv_filename_prefix = "D:/handwriting from motion/aayush/9"
+counter = 0
+while True:
+    csv_filename = f"{csv_filename_prefix}.csv"
+    if not os.path.exists(csv_filename):
+        break
+    counter += 1
+csvfile = open(csv_filename, 'w', newline='')
+csvwriter = csv.writer(csvfile)
+
+# Write header
+csvwriter.writerow(['accelX', 'accelY', 'accelZ', 'gyroX', 'gyroY', 'gyroZ'])
+print("Recording...")
+
+try:
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').strip()
+            if line == "Recording Stopped":
+                print("Done recording.")
+                break
+            elif line == "Recording Started":
+                 print("Started")
+            elif line == "Ready":
+                 print("Waiting to record...")
+            else:
+                try:
+                    values = line.split(',')
+                    values = [float(val) for val in values]
+                    csvwriter.writerow(values)
+                except ValueError:
+                     pass # Skip invalid lines
+
+except KeyboardInterrupt:
+    print("\nSaving...")
+finally:
+    ser.close()
+    csvfile.close()
--- script/script.ino
+++ script/script.ino
@@ -3,7 +3,8 @@
 
 #define IMU_ADDRESS 0x68    //Change to the address of the IMU
 #define PERFORM_CALIBRATION //Comment to disable startup calibration
-MPU6500 IMU;               //Change to the name of any supported IMU! 
+#define BUTTON_PIN 5       // Define the button pin
+MPU6500 IMU;               //Change to the name of any supported IMU!
 
 // Currently supported IMUS: MPU9255 MPU9250 MPU6886 MPU6500 MPU6050 ICM20689 ICM20690 BMI055 BMX055 BMI160 LSM6DS3 LSM6DSL QMI8658
 
@@ -11,6 +12,12 @@ calData calib = { 0 };  //Calibration data
 AccelData accelData;    //Sensor data
 GyroData gyroData;
 MagData magData;
+
+bool start_data_transmission = false;
+long lastPrintTime = 0;
+const int samplingRate = 20; // Sample every 50ms
+bool buttonState = false;  // Current state of the button
+bool lastButtonState = false; // Previous state of the button
 
 void setup() {
   Wire.begin();
@@ -20,6 +27,7 @@ void setup() {
     ;
   }
 
+  pinMode(BUTTON_PIN, INPUT_PULLUP);  // Initialize button pin with internal pull-up resistor
   int err = IMU.init(calib, IMU_ADDRESS);
   if (err != 0) {
     Serial.print("Error initializing IMU: ");
@@ -28,58 +36,21 @@ void setup() {
       ;
     }
   }
-  
-#ifdef PERFORM_CALIBRATION
-  Serial.println("FastIMU calibration & data example");
-  if (IMU.hasMagnetometer()) {
-    delay(1000);
-    Serial.println("Move IMU in figure 8 pattern until done.");
-    delay(3000);
-    IMU.calibrateMag(&calib);
-    Serial.println("Magnetic calibration done!");
-  }
-  else {
-    delay(5000);
-  }
 
-  delay(5000);
+#ifdef PERFORM_CALIBRATION
   Serial.println("Keep IMU level.");
-  delay(5000);
+  delay(3000);
   IMU.calibrateAccelGyro(&calib);
   Serial.println("Calibration done!");
-  Serial.println("Accel biases X/Y/Z: ");
-  Serial.print(calib.accelBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.accelBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.accelBias[2]);
-  Serial.println("Gyro biases X/Y/Z: ");
-  Serial.print(calib.gyroBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.gyroBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.gyroBias[2]);
-  if (IMU.hasMagnetometer()) {
-    Serial.println("Mag biases X/Y/Z: ");
-    Serial.print(calib.magBias[0]);
-    Serial.print(", ");
-    Serial.print(calib.magBias[1]);
-    Serial.print(", ");
-    Serial.println(calib.magBias[2]);
-    Serial.println("Mag Scale X/Y/Z: ");
-    Serial.print(calib.magScale[0]);
-    Serial.print(", ");
-    Serial.print(calib.magScale[1]);
-    Serial.print(", ");
-    Serial.println(calib.magScale[2]);
-  }
-  delay(5000);
+  calib.accelBias[0];
+  calib.accelBias[1];
+  calib.accelBias[2];
+  calib.gyroBias[0];
+  calib.gyroBias[1];
+  calib.gyroBias[2];
   IMU.init(calib, IMU_ADDRESS);
 #endif
 
-  //err = IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
-  //err = IMU.setAccelRange(2);       //THESE TWO SET THE GYRO RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g
-  
   if (err != 0) {
     Serial.print("Error Setting range: ");
     Serial.println(err);
@@ -87,38 +58,48 @@ void setup() {
       ;
     }
   }
+  Serial.println("Ready"); // Send ready message
 }
 
 void loop() {
-  IMU.update();
-  IMU.getAccel(&accelData);
-  Serial.print(accelData.accelX);
-  Serial.print("\t");
-  Serial.print(accelData.accelY);
-  Serial.print("\t");
-  Serial.print(accelData.accelZ);
-  Serial.print("\t");
-  IMU.getGyro(&gyroData);
-  Serial.print(gyroData.gyroX);
-  Serial.print("\t");
-  Serial.print(gyroData.gyroY);
-  Serial.print("\t");
-  Serial.print(gyroData.gyroZ);
-  if (IMU.hasMagnetometer()) {
-    IMU.getMag(&magData);
-    Serial.print("\t");
-    Serial.print(magData.magX);
-    Serial.print("\t");
-    Serial.print(magData.magY);
-    Serial.print("\t");
-    Serial.print(magData.magZ);
+
+  buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState != lastButtonState) {
+    if (buttonState == LOW) { // Button pressed (LOW because of pullup)
+      start_data_transmission = !start_data_transmission; // Toggle data transmission
+      if(start_data_transmission){
+        Serial.println("Recording Started");
+        lastPrintTime= millis();
+      }else{
+         Serial.println("Recording Stopped");
+      }
+       
+    }
+    lastButtonState = buttonState;  // Update the previous button state
+    delay(50); // Simple debounce delay
   }
-  if (IMU.hasTemperature()) {
-	  Serial.print("\t");
-	  Serial.println(IMU.getTemp());
-  }
-  else {
-    Serial.println();
-  }
-  delay(50);
+  
+   if (start_data_transmission){
+
+     if(millis() - lastPrintTime >= (1000 / samplingRate)) //Sampling rate
+     {
+
+        IMU.update();
+        IMU.getAccel(&accelData);
+        Serial.print(accelData.accelX-calib.accelBias[0]);
+        Serial.print(",");
+        Serial.print(accelData.accelY-calib.accelBias[1]);
+        Serial.print(",");
+        Serial.print(accelData.accelZ-calib.accelBias[2]);
+        Serial.print(",");
+        IMU.getGyro(&gyroData);
+        Serial.print(gyroData.gyroX-calib.gyroBias[0]);
+        Serial.print(",");
+        Serial.print(gyroData.gyroY-calib.gyroBias[1]);
+        Serial.print(",");
+        Serial.print(gyroData.gyroZ-calib.gyroBias[2]);
+        Serial.print("\n");
+        lastPrintTime = millis();
+       }
+   }
 }
